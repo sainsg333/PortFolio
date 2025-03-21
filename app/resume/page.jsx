2af97ca0136a51ab571ae3b5556b893153a80ca4
sainsg333 <112270968+sainsg333@users.noreply.github.com>
2025-03-21 19:57:22 +0530

    Update page.jsx

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -4,6 +4,8 @@ import { Description } from "@radix-ui/react-dialog";
 import { Instrument_Sans } from "next/font/google";
 import { FaHtml5,FaCss3,FaJs,FaReact,FaNodeJs } from "react-icons/fa";
 import { SiTailwindcss,SiNextdotjs } from "react-icons/si";
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs, FaBrain, FaEye, FaMobileAlt } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
 
 
 const about={
